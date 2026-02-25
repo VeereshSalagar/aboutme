dcfda1d13fdafabbd9f1e9eca09230ef146c018e
VeereshSalagar <veereshsss784@gmail.com>
2026-02-25 23:07:39 +0530

    Sync parent repo with latest portfolio structure and subrepo pointer

---
+++ AboutMePortfolio/Script.js
@@ -0,0 +1,13 @@
+AOS.init();
+
+var typed = new Typed("#typing", {
+    strings: [
+        "Full Stack Software Test Engineer",
+        "Selenium Automation Specialist",
+        "IV&V Engineer",
+        "QA Professional"
+    ],
+    typeSpeed: 50,
+    backSpeed: 30,
+    loop: true
+});
