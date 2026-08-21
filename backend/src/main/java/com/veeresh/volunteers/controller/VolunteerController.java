01f9571e419ffcc3652203552e950d37ede26bea
VeereshSalagar <veereshsss784@gmail.com>
2026-08-21 20:34:13 +0530

    Update CORS allowed origins to include 127.0.0.1 and localhost

--- backend/src/main/java/com/veeresh/volunteers/controller/VolunteerController.java
+++ backend/src/main/java/com/veeresh/volunteers/controller/VolunteerController.java
@@ -10,8 +10,12 @@ import jakarta.validation.Valid;
 
 @CrossOrigin(
     origins = {
-        "https://veereshsalagar.github.io",
-        "http://localhost:3000"
+        
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
+        "https://veereshsalagar.github.io"
     },
     allowedHeaders = "*",
     methods = {
